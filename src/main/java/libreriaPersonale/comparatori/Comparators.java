9e7ca58a39b07799301f270a093ad27449db8e5c
Pierfrancesco Fasanella <fsnpfr04c31c349n@studenti.unical.it>
2025-05-29 13:07:42 +0200

    Ora i Comparators sono case insensitive

--- src/main/java/libreriaPersonale/comparatori/Comparators.java
+++ src/main/java/libreriaPersonale/comparatori/Comparators.java
@@ -9,11 +9,11 @@ public class Comparators {
 
     //public static Comparator<Libro> perISBN = Comparator.comparing(Libro::getIsbn, Comparator.nullsLast(String::compareTo));
 
-    public static Comparator<Libro> perTitolo = Comparator.comparing(Libro::getTitolo, Comparator.nullsLast(String::compareTo));
+    public static Comparator<Libro> perTitolo = Comparator.comparing(Libro::getTitolo, Comparator.nullsLast(String::compareToIgnoreCase));
 
-    public static Comparator<Libro> perAutore = Comparator.comparing(Libro::getAutore, Comparator.nullsLast(String::compareTo));
+    public static Comparator<Libro> perAutore = Comparator.comparing(Libro::getAutore, Comparator.nullsLast(String::compareToIgnoreCase));
 
-    public static Comparator<Libro> perGenere = Comparator.comparing(Libro::getGenere, Comparator.nullsLast(String::compareTo));
+    public static Comparator<Libro> perGenere = Comparator.comparing(Libro::getGenere, Comparator.nullsLast(String::compareToIgnoreCase));
 
     public static Comparator<Libro> perValutazione = Comparator.comparingInt(Libro::getValutazione);
 
